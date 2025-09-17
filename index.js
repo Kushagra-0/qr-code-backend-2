b20de451f34ebc8c3a4986a77aa03d83d867bb01
Kushagra-0 <kushgarashukla.dev@gmail.com>
2025-09-17 14:57:45 +0530

    made changes in index

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ app.use('/api/blog', blogRoutes);
 app.use('/api/qrcodes', qrCodeRoutes);
 app.use('/api/upload', uploadRoutes); 
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`App listening at http://localhost:${port}`);
 });
